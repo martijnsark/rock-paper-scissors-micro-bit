02c026b20cd1bf9481509de24c714ffe29dcdaef
martiijn <94526855+martijnsark@users.noreply.github.com>
2022-09-19 13:11:57 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,63 @@
+let item = 0
+input.onButtonPressed(Button.A, function () {
+    item = randint(1, 3)
+    if (item == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.clearScreen()
+    } else if (item == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        basic.clearScreen()
+    } else if (item == 3) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    item = randint(1, 3)
+    if (item == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.clearScreen()
+    } else if (item == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        basic.clearScreen()
+    } else if (item == 3) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+        basic.clearScreen()
+    }
+})
